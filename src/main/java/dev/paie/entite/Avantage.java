47cb0b3b1429bb44447b66d756e46004f5c573a3
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-03-30 08:43:39 +0200

    init workshop

---
+++ src/main/java/dev/paie/entite/Avantage.java
@@ -0,0 +1,43 @@
+package dev.paie.entite;
+
+import java.math.BigDecimal;
+
+public class Avantage {
+
+	private Integer id;
+	private String code;
+	private String nom;
+	private BigDecimal montant;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public BigDecimal getMontant() {
+		return montant;
+	}
+
+	public void setMontant(BigDecimal montant) {
+		this.montant = montant;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
